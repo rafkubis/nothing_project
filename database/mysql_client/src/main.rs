090e7d9417067de7a7f8a64d524f3aa7934cd74a
Rafal <rafal9511k@gmail.com>
2024-08-31 16:46:13 +0200

    async mqtt

--- database/mysql_client/src/main.rs
+++ database/mysql_client/src/main.rs
@@ -1,6 +1,8 @@
 extern crate paho_mqtt as mqtt;
 use chrono;
 use env_logger::{Builder, Target};
+use log::info;
+use mqtt::SuccessCallback;
 use mysql::serde_json;
 use std::env;
 use std::fs::File;
@@ -15,63 +17,67 @@ fn main() {
     if args.len() > 1 {
         let name = env!("CARGO_PKG_NAME");
         let log_filepath = args[1].to_owned() + "/" + name + ".log";
-        let file = Box::new(File::create(log_filepath).unwrap());
+        let file: Box<File> = Box::new(File::create(log_filepath).unwrap());
         Builder::from_default_env()
             .target(Target::Pipe(file))
             .init();
     } else {
-        Builder::from_default_env().target(Target::Stdout).init(); 
+        Builder::from_default_env()
+            .target(Target::Stdout)
+            .filter_level(log::LevelFilter::Debug)
+            .init();
     }
-
     log::info!("Starting application");
 
     let mut conn = database::open_sql_connection();
     create_table_if_not_exist(&mut conn);
 
     let cli = connect_and_publish_message();
-
-    let sub: mqtt::ServerResponse = cli.subscribe("test/topic", 0).expect("R");
-    let receiver = cli.start_consuming();
-    receiver.iter().for_each(|msg| match msg {
-        None => log::debug!("None"),
-        Some(value) => {
-            let parsed = serde_json::from_str::<rest::Root>(value.payload_str().as_ref());
-            match parsed {
-                Ok(parsed) => {
-                    log::info!("Parsed: {:?}", parsed);
-
-                    let temperature = calculate_temperature(parsed);
-                    let formatted_date_time =
-                        chrono::Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
-                    drop_querry(&mut conn, temperature, &formatted_date_time);
-                }
-                Err(e) => {
-                    log::error!("Error: {:?}", e);
-                }
-            }
-        }
+    let token= cli.subscribe("test/topic", mqtt::QOS_1);
+    cli.set_message_callback(|cli, msg| {
+        log::info!("message callback");
     });
+    while true{
+
+    }
+
+
 
     println!("Hello, world!");
 }
 
-fn connect_and_publish_message() -> mqtt::Client {
-    let cli = mqtt::Client::new("tcp://mosquitto:1883").unwrap();
+fn connect_and_publish_message() -> mqtt::AsyncClient {
+    let cli = mqtt::AsyncClient::new("tcp://mosquitto:1883").unwrap();
+
+    let lwt = mqtt::Message::new(
+        "test/lwt",
+        "[LWT] Async subscriber v5 lost connection",
+        mqtt::QOS_1,
+    );
 
     let conn_opts = mqtt::ConnectOptionsBuilder::new()
         .keep_alive_interval(Duration::from_secs(20))
         .clean_session(true)
+        .will_message(lwt)
         .finalize();
 
-    if let Err(e) = cli.connect(conn_opts) {
-        log::debug!("Unable to connect:\n\t{:?}", e);
-    }
+    cli.connect_with_callbacks(
+        conn_opts,
+        |cli, result| {
+            log::info!("Success callback {result}");
+        },
+        |cli, result, reslut2| {
+            log::info!("Failure callback {result}, {reslut2}");
+        },
+    );
 
-    let msg = mqtt::Message::new("test", "Hello world!", 0);
-    if let Err(e2) = cli.publish(msg) {
-        log::debug!("Unable to publish message:\n\t{:?}", e2);
-    }
 
+
+    /*    let msg = mqtt::Message::new("test", "Hello world!", 0);
+        if let Err(e2) = cli.publish(msg) {
+            log::debug!("Unable to publish message:\n\t{:?}", e2);
+        }
+    */
     cli
 }
 
